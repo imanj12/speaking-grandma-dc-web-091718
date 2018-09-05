d16406c04897b332e68948883a92f1d7e8772bdf
Iman Jawad <imanj12@gmail.com>
2018-09-05 17:02:04 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -13,5 +13,10 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(words)
-  if (words == words.)
+  if (words == "I LOVE YOU GRANDMA!")
+    return "I LOVE YOU TOO PUMPKIN!"
+  elsif (words == words.upcase)
+    return "NO, NOT SINCE 1938!"
+  else 
+    return "HUH?! SPEAK UP, SONNY!"
 end
